f0b18f167b0abc75fb25a6c5ec92787e2515619c
polina1201 <polina.melvin1201@gmail.com>
2022-10-04 19:37:58 -0400

    Class13

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-StringBuilder stringBuilder=new StringBuilder("Intellij is great");
-        System.out.println(stringBuilder);
-        stringBuilder.reverse();
-        System.out.println(stringBuilder);
-    }
-}
