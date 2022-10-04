eddcd9ad75a2cc28d4df9bb7487de2cdcf7bccc3
polina1201 <polina.melvin1201@gmail.com>
2022-10-04 19:43:46 -0400

    Class 15

---
+++ src/Class16/Task1.java
@@ -0,0 +1,10 @@
+package Class16;
+
+public class Task1 {
+
+    //Create a Class called SyntaxEmployee: Create 3 variables empID, Salary, and set the CEO to "Sumair"
+
+    String empID;
+    Double salary;
+    static String CEO="Sumair";
+}
