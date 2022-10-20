418e1a382c8ceb4c66bc87927a10a3b04aa71dae
polina1201 <polina.melvin1201@gmail.com>
2022-10-20 19:12:47 -0400

    new class

--- src/Practice/Repls.java
+++ src/Practice/Repls.java
@@ -6,14 +6,14 @@ public class Repls {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("In:");
-        String givenString = inp.nextLine();
+        String s = inp.nextLine();
+        //write your code below
+      for (int i=s.length()-1; i>=0; i--){
 
-    StringBuilder poly=new StringBuilder();
-    poly.reverse();
-if (poly.toString().equals(givenString)){
-    System.out.println("true");
+
+            System.out.print(s.charAt(i));
+        }
     }
-else {
-    System.out.println("false");
+
+
 }
-}}
